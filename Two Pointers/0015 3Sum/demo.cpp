3c62dc17c4790047119aec7fc1ec273a7996cc23
charlesyifanli <liyifan1400@gmail.com>
2023-12-12 18:51:51 +0800

    update *

--- Two Pointers/0015 3Sum/demo.cpp
+++ Two Pointers/0015 3Sum/demo.cpp
@@ -1,21 +1,21 @@
 // #include <algorithm>
 // #include <iostream>
 // #include <vector>
-
+//
 // using namespace std;
-
+//
 // vector<vector<int>> threeSum(vector<int> &nums) {
 //     vector<vector<int>> result;
 //     sort(nums.begin(), nums.end());
 //     int length = nums.size();
-
+//
 //     for (int index = 0; index < length - 2; index++) {
 //         if (index > 0 && nums[index] == nums[index - 1]) {
 //             continue;
 //         }
-
+//
 //         int start = index + 1, end = length - 1;
-
+//
 //         while (start < end) {
 //             int sum = nums[index] + nums[start] + nums[end];
 //             if (sum == 0) {
@@ -35,21 +35,21 @@
 //             }
 //         }
 //     }
-
+//
 //     return result;
 // }
-
+//
 // int main() {
 //     vector<int> arr = {-1, 0, 1, 2, -1, -4};
 //     vector<vector<int>> arr1 = threeSum(arr);
-
+//
 //     for (const auto &triplet : arr1) {
 //         for (int num : triplet) {
 //             cout << num << " ";
 //         }
 //         cout << endl;
 //     }
-
+//
 //     return 0;
 // }
-
+//
