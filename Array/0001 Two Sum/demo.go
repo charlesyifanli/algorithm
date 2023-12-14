d02cc47fb11904dffacfbf03ceb352451456db8e
charlesyifanli <liyifan1400@gmail.com>
2023-12-14 19:49:16 +0800

    update 0001

--- Array/0001 Two Sum/demo.go
+++ Array/0001 Two Sum/demo.go
@@ -2,18 +2,6 @@ package main
 
 import "fmt"
 
-func twoSum(nums []int, target int) []int {
-	m := make(map[int]int)
-	for i := 0; i < len(nums); i++ {
-		another := target - nums[i]
-		if _, ok := m[another]; ok {
-			return []int{m[another], i}
-		}
-		m[nums[i]] = i
-	}
-	return nil
-}
-
 func main() {
 	nums := []int{2, 7, 11, 15}
 	target := 9
@@ -25,3 +13,29 @@ func main() {
 		fmt.Println("No such pair found.")
 	}
 }
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		diff := target - nums[i]
+		if _, ok := m[diff]; ok {
+			return []int{i, m[diff]}
+		}
+		m[nums[i]] = i
+	}
+	return nil
+}
+
+/*
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		another := target - nums[i]
+		if _, ok := m[another]; ok {
+			return []int{m[another], i}
+		}
+		m[nums[i]] = i
+	}
+	return nil
+}
+*/
