c01fa6ce4d1221a69b81bb4efdb58f661ce95301
charlesyifanli <liyifan1400@gmail.com>
2023-12-15 21:48:32 +0800

    update 0001

--- Array/0001 Two Sum/demo.py
+++ Array/0001 Two Sum/demo.py
@@ -1,17 +1,13 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         num_dict = {}
-
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_dict:
-                return [num_dict[complement], i]
-            num_dict[num] = i
-
-        return []  # Return an empty list if no matching indices are found
+        for index, num in enumerate(nums):
+            if target - num in num_dict:
+                return [num_dict[target - num], index]
+            num_dict[num] = index
+        return []
 
 
-# Example usage:
 solution = Solution()
 nums = [2, 7, 11, 15]
 target = 9
