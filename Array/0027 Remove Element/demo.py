9290424148960664406ac22a33601be781bd62f3
charlesyifanli <liyifan1400@gmail.com>
2023-12-22 19:47:37 +0800

    create README.md in 0027

---
+++ Array/0027 Remove Element/demo.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution(object):
+    def removeElement(self, nums: List[int], val: int) -> int:
+        return 0
+
+
+if __name__ == '__main__':
+    solution = Solution()
