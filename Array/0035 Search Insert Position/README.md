c9cc1581a7517831386fc59246a9de20fd505b96
charlesyifanli <liyifan1400@gmail.com>
2023-12-22 21:34:02 +0800

    update 0035

--- Array/0035 Search Insert Position/README.md
+++ Array/0035 Search Insert Position/README.md
@@ -1,9 +1,15 @@
 ## understanding
 
+Input: 严格升序（无重复元素）整数数组，目标值<br>
+Operation:时间复杂度小于等于O(log n)<br>
+Output: 目标值的下标或要被插在哪里
+
 ## demo
 
+二分查找
+
 ```
-time complexity：O(n)
+time complexity：O(log n)
 space complexity: O(1)
 ```
 
---
+++ Array/0035 Search Insert Position/demo.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution(object):
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r, mid = 0, len(nums) - 1, 0
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        if r > mid:
+            return r
+        else:
+            return l
+
+
+if __name__ == '__main__':
+    case, val = [1, 3, 5, 6], 5
+    print(Solution().searchInsert(case, val))
+
+    case, val = [1, 3, 5, 6], 2
+    print(Solution().searchInsert(case, val))
+
+    case, val = [1, 3, 5, 6], 7
+    print(Solution().searchInsert(case, val))
