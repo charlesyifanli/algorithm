2915687e5ba6084a6d39dd1acd3833e6cd44c96a
charlesyifanli <liyifan1400@gmail.com>
2023-12-22 12:43:25 +0800

    update 0026

--- Array/0026 Remove Duplicates from Sorted Array/demo.py
+++ Array/0026 Remove Duplicates from Sorted Array/demo.py
@@ -3,17 +3,10 @@ from typing import Tuple, List, Set, Dict, Optional
 
 class Solution(object):
     def removeDuplicates(self, nums: List[int]) -> int:
-        new_nums = []
-
-        if not nums:
-            return 0
-        else:
-            for i in range(0, len(nums)):
-                if not new_nums:
-                    new_nums.append(nums[0])
-                if new_nums[-1] != nums[i]:
-                    new_nums.append(nums[i])
-
+        new_nums = [nums[0]]
+        for i in range(0, len(nums)):
+            if new_nums[-1] != nums[i]:
+                new_nums.append(nums[i])
         for i in range(len(new_nums)):
             nums[i] = new_nums[i]
         return len(new_nums)
---
+++ Array/0026 Remove Duplicates from Sorted Array/demo_2.py
@@ -0,0 +1,9 @@
+from typing import Tuple, List, Set, Dict, Optional
+
+
+class Solution(object):
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i, j = 0, 1
+
+
+        return 0
