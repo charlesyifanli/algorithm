9b2eff8eb051a9bd5906c25e88daf16b0b9b76d9
charlesyifanli <liyifan1400@gmail.com>
2023-12-25 16:31:54 +0800

    update 0118

---
+++ Array/0118 Pascal's Triangle/demo.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution(object):
+    def generate(self, numRows: int) -> List[List[int]]:
+        return []
+
+
+if __name__ == '__main__':
+    numRows = 1
+    print(Solution().generate(numRows))
+
+    numRows = 4
+    print(Solution().generate(numRows))
+
+    numRows = 5
+    print(Solution().generate(numRows))
