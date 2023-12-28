59267cc9a2472e903e41bf86f185f7037b705d27
charlesyifanli <liyifan1400@gmail.com>
2023-12-28 18:49:39 +0800

    update 0234

--- Linked List/0234 Palindrome Linked List/demo.py
+++ Linked List/0234 Palindrome Linked List/demo.py
@@ -9,3 +9,18 @@ class ListNode:
 
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        curr = head
+        ls = []
+        while curr:
+            ls.append(curr.val)
+            curr = curr.next
+        return ls[:len(ls) >> 1] == ls[:-(len(ls) >> 1) - 1:-1]
+
+
+if __name__ == '__main__':
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(2)
+    head.next.next.next.next = ListNode(1)
+    print(Solution().isPalindrome(head))
