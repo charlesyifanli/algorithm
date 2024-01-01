d02e61d1c72019b05289505239ea582ad19a7fb6
charlesyifanli <liyifan1400@gmail.com>
2024-01-01 19:15:37 +0800

    update test

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 class Solution(object):
-    def only_letter_num(self, s: str) -> str:
+    @staticmethod
+    def f(s: str) -> str:
         return ''.join(x for x in s if x.isnumeric())
 
 
 if __name__ == '__main__':
-    print(Solution().only_letter_num("hello! 3 wo2rld!"))
+    print(Solution.f('a1b2c3d4e5f6g7h8i9j0'))
