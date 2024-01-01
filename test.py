00274c608d040fae9648fa6bbcdda97484175a38
charlesyifanli <liyifan1400@gmail.com>
2024-01-01 22:03:32 +0800

    delete test

--- test.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    @staticmethod
-    def f(s: str) -> str:
-        return ''.join(x for x in s if x.isnumeric())
-
-
-if __name__ == '__main__':
-    print(Solution.f('a1b2c3d4e5f6g7h8i9j0'))
