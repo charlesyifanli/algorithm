091b3e967bc0bfb51e526a95770d2a8cc5658660
charlesyifanli <liyifan1400@gmail.com>
2024-01-03 11:36:09 +0800

    update 0303

--- Dynamic Programming/0303 Range Sum Query - Immutable/demo.py
+++ Dynamic Programming/0303 Range Sum Query - Immutable/demo.py
@@ -1,8 +1,21 @@
 from typing import List
 
 
+# Prefix Sum
 class NumArray(object):
+    def __init__(self, nums: List[int]):
+        self.sums = [0]
+        _sums = self.sums
 
+        for num in nums:
+            _sums.append(_sums[-1] + num)
+
+    def sumRange(self, i: int, j: int) -> int:
+        _sums = self.sums
+        return _sums[j + 1] - _sums[i]
+
+
+'''
     def __init__(self, nums: List[int]):
         self.nums = nums
 
@@ -11,6 +24,8 @@ class NumArray(object):
         if left > right: return 0
         if left == right: return self.nums[left]
         return curr_num + self.sumRange(left + 1, right - 1)
+    Worse time complexity: from O(n) to O(n log n)!!!
+'''
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
