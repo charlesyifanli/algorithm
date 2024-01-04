0cca5b63865b8d0ad9984455ddf8f68b0a9dbdd4
charlesyifanli <liyifan1400@gmail.com>
2024-01-04 16:51:06 +0800

    update 0392

--- Dynamic Programming/0392 Is Subsequence/demo.py
+++ Dynamic Programming/0392 Is Subsequence/demo.py
@@ -1,3 +1,13 @@
 class Solution(object):
     def isSubsequence(self, s: str, t: str) -> bool:
+        for _, val in enumerate(s):
+            if val in t:
+                t = t[t.index(val)+1:]
+                continue
+            else:
+                return False
+        return True
 
+
+if __name__ == '__main__':
+    print(Solution().isSubsequence("aaaaaa", "bbaaaa"))
