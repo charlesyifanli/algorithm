56c53ead8051acd87d6d2667065106188e82acbe
charlesyifanli <liyifan1400@gmail.com>
2024-01-04 20:37:39 +0800

    update 0100

--- DFS/0100 Same Tree/demo.py
+++ DFS/0100 Same Tree/demo.py
@@ -10,5 +10,5 @@ class TreeNode(object):
 
 class Solution(object):
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p is None and q is None: return True
+        if not p or not q: return p == q
         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
