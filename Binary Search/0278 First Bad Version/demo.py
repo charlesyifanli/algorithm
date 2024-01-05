5134b82a43e602b74cf442c75148bb16ad76101d
charlesyifanli <liyifan1400@gmail.com>
2024-01-05 09:24:16 +0800

    update 0278

--- Binary Search/0278 First Bad Version/demo.py
+++ Binary Search/0278 First Bad Version/demo.py
@@ -1,10 +1,18 @@
 # The isBadVersion API is already defined for you.
 
+def isBadVersion(version: int) -> bool:
+    return False
+
 
 class Solution(object):
     def firstBadVersion(self, n: int) -> int:
-
-        return 0
-
-    def isBadVersion(version: int) -> bool:
-        return False
+        left, right, res = 1, n, 0
+        while left <= right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                right = mid - 1
+                res = right + 1
+            else:
+                left = mid + 1
+                res = left
+        return res
