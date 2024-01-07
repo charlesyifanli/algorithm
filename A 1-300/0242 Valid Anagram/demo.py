821d0928e42388efd64b6040867545bb87ff3354
charlesyifanli <liyifan1400@gmail.com>
2024-01-07 21:44:08 +0800

    update 0242

---
+++ A 1-300/0242 Valid Anagram/demo.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def isAnagram(self, s: str, t: str) -> bool:
+        dict_ = {}
+        for _, val in enumerate(s):
+            dict_[val] = 1 if val not in dict_ else dict_[val] + 1
+        for _, val in enumerate(t):
+            if val not in dict_: return False
+            dict_[val] -= 1
+        set_ = dict_.values()
+        for val in set_:
+            if val != 0: return False
+        return True
+
+
+if __name__ == '__main__':
+    def test_is_anagram():
+        # case
+        s = 'abc'
+        t = 'bca'
+        assert Solution().isAnagram(s, t) == True
+
+        # case
+        s = 'abc'
+        t = 'abca'
+        assert Solution().isAnagram(s, t) == False
+
+        print('Succeed')
+
+
+    test_is_anagram()
