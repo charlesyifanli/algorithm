9af0b4471cda6e5f12634669f0dad9b9eb307fc1
charlesyifanli <liyifan1400@gmail.com>
2024-01-11 20:28:32 +0800

    update 1672

---
+++ F 1501-1800/1632 Richest Customer Wealth/demo.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution(object):
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        max_wealth = 0
+        for obj in accounts:
+            temp = 0
+            for val in obj: temp += val
+            max_wealth = max(max_wealth, temp)
+        return max_wealth
