6ec81484dd35ee00469ce9d3c22dfd49ceaa412f
charlesyifanli <liyifan1400@gmail.com>
2024-01-12 19:55:11 +0800

    update 0125

--- A 1-300/0125 Valid Palindrome/demo.py
+++ A 1-300/0125 Valid Palindrome/demo.py
@@ -4,7 +4,7 @@ import re
 class Solution(object):
     def isPalindrome(self, s: str) -> bool:
         s = ''.join(x for x in s if x.isalnum()).upper()
-        return s[:len(s) >> 1] == s[:-(len(s) >> 1) - 1:-1]
+        return s == s[::-1]
 
 
 '''
@@ -36,11 +36,17 @@ class Solution(object):
 '''
 
 if __name__ == '__main__':
-    case = "A man, a plan, a canal: Panama"
-    print(Solution().isPalindrome(case))
+    def test_is_palindrome():
+        case = "A man, a plan, a canal: Panama"
+        assert Solution().isPalindrome(case) == True
 
-    case = "race a car"
-    print(Solution().isPalindrome(case))
+        case = "race a car"
+        assert Solution().isPalindrome(case) == False
 
-    case = "0P"
-    print(Solution().isPalindrome(case))
+        case = "0P"
+        assert Solution().isPalindrome(case) == False
+
+        print('Succeed')
+
+
+    test_is_palindrome()
