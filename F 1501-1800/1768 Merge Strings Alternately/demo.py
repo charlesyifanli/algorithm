1e70963c0525baebd0c24ac91ed08f505bfca4c9
charlesyifanli <liyifan1400@gmail.com>
2024-01-12 19:49:09 +0800

    update 1768

--- F 1501-1800/1768 Merge Strings Alternately/demo.py
+++ F 1501-1800/1768 Merge Strings Alternately/demo.py
@@ -10,6 +10,24 @@ class Solution(object):
             j += 1
         return str_
 
+    '''
+        def mergeAlternately(self, word1: str, word2: str) -> str:
+        p = q = 0
+        str_ = ''
+        while p < len(word1) or q < len(word2):
+            if p == len(word1):
+                str_ += word2[q:]
+                continue
+            if q == len(word2):
+                str_ += word1[p:]
+                continue
+            str_ += word1[p] + word2[q]
+            p += 1
+            q += 1
+        return str_
+        # Time Limit Exceeded
+    '''
+
 
 if __name__ == '__main__':
     def test_merge_alternately():
