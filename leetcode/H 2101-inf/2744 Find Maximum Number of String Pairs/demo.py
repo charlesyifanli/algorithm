f9d276287d52fa25ce605acf989f00f9b20a41ad
charlesyifanli <liyifan1400@gmail.com>
2024-01-17 22:22:52 +0800

    update 2744

--- leetcode/H 2101-inf/2744 Find Maximum Number of String Pairs/demo.py
+++ leetcode/H 2101-inf/2744 Find Maximum Number of String Pairs/demo.py
@@ -3,11 +3,26 @@ from typing import List
 
 class Solution(object):
     def maximumNumberOfStringPairs(self, words: List[str]) -> int:
-        return 0
+        set_ = set()
+        res = 0
+        for val in words:
+            if val[::-1] in set_:
+                res += 1
+            else:
+                set_.add(val)
+        return res
 
 
 if __name__ == '__main__':
     def test():
+        # case
+        words = ["cd", "ac", "dc", "ca", "zz"]
+        assert Solution().maximumNumberOfStringPairs(words) == 2
+
+        # case
+        words = ["ab", "ba", "cc"]
+        assert Solution().maximumNumberOfStringPairs(words) == 1
+
         print('Succeed')
 
 
