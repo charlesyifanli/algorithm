f41ab74b04130e9679438b16f123e83eea88b7d5
charlesyifanli <liyifan1400@gmail.com>
2024-01-17 21:53:02 +0800

    update 1207

---
+++ leetcode/E 1201-1500/1207 Unique Number of Occurrences/demo.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution(object):
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        return False
+
+
+if __name__ == '__main__':
+    def test():
+        print('Succeed')
+
+
+    test()
