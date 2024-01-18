aa4127780836b4ecdf0f6082b83a49c370788ae8
charlesyifanli <liyifan1400@gmail.com>
2024-01-18 14:09:54 +0800

    update 0070

--- leetcode/A 1-300/0070 Climbing Stairs/demo.py
+++ leetcode/A 1-300/0070 Climbing Stairs/demo.py
@@ -1,10 +1,20 @@
 class Solution(object):
     def climbStairs(self, n: int) -> int:
+        if n == 1: return 1
+        if n == 2: return 2
+        a, b, temp = 1, 2, 0
+        for i in range(3, n + 1):
+            a, b = b, a + b
+        return b
+
+
+'''
+    def climbStairs(self, n: int) -> int:
         a, b = 1, 1
         for _ in range(n - 1):
             a, b = b, a + b
         return b
-
+'''
 
 '''
     def climbStairs(self, n: int) -> int:
