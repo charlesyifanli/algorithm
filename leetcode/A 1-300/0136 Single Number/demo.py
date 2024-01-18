506c8648cf8679012de8f64146c326c47b3880fa
charlesyifanli <liyifan1400@gmail.com>
2024-01-18 19:42:10 +0800

    update 0136

---
+++ leetcode/A 1-300/0136 Single Number/demo.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution(object):
+    def singleNumber(self, nums: List[int]) -> int:
+        return 0
+
+
+if __name__ == '__main__':
+    def test():
+        print('Success')
+
+
+    test()
