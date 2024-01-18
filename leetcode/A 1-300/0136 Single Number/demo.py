8497a942e0c9547dae5287710fd10c9ef2dde6d8
charlesyifanli <liyifan1400@gmail.com>
2024-01-18 19:50:45 +0800

    update 0136

--- leetcode/A 1-300/0136 Single Number/demo.py
+++ leetcode/A 1-300/0136 Single Number/demo.py
@@ -3,11 +3,22 @@ from typing import List
 
 class Solution(object):
     def singleNumber(self, nums: List[int]) -> int:
-        return 0
+        res = 0
+        for val in nums:
+            res ^= val
+        return res
 
 
 if __name__ == '__main__':
     def test():
+        # case
+        nums = [2, 2, 1]
+        assert Solution().singleNumber(nums) == 1
+
+        # case
+        nums = [4, 1, 2, 1, 2]
+        assert Solution().singleNumber(nums) == 4
+
         print('Success')
 
 
