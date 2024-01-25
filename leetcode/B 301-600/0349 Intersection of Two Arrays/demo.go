31d6eeef72f0b7321ea7d43d5165bf3c12fb852e
Charles <liyifan1400@gmail.com>
2024-01-25 18:41:00 +0800

    Create demo.go

---
+++ leetcode/B 301-600/0349 Intersection of Two Arrays/demo.go
@@ -0,0 +1,38 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"sort"
+// )
+
+func intersection(nums1 []int, nums2 []int) []int {
+	set1 := make(map[int]bool)
+	for _, num := range nums1 {
+		set1[num] = true
+	}
+
+	set2 := make(map[int]bool)
+	for _, num := range nums2 {
+		if _, ok := set1[num]; ok {
+			set2[num] = true
+		}
+	}
+
+	res := make([]int, 0, len(set2))
+	for num := range set2 {
+		res = append(res, num)
+	}
+
+	sort.Ints(res)
+	return res
+}
+
+// func main() {
+// 	nums1 := []int{1, 2, 2, 1}
+// 	nums2 := []int{2, 2}
+// 	fmt.Println(intersection(nums1, nums2))
+
+// 	nums1 = []int{4, 9, 5}
+// 	nums2 = []int{9, 4, 9, 8, 4}
+// 	fmt.Println(intersection(nums1, nums2))
+// }
