8ccb4c53c2dc5a643837abc441252c08dc149bdc
charlesyifanli <liyifan1400@gmail.com>
2024-01-25 15:02:01 +0800

    update leetcode--455

---
+++ leetcode/B 301-600/0455 Assign Cookies/demo.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution(object):
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        res = 0
+        return res
+
+
+if __name__ == '__main__':
+    def test():
+        # case
+        print('Succeed')
+
+
+    test()
