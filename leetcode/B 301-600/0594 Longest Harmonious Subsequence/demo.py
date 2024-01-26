3f9e3ba7eb16b1d3658a0dfe4e1c23e82bf6ca46
charlesyifanli <liyifan1400@gmail.com>
2024-01-26 23:59:37 +0800

    update leetcode--594

--- leetcode/B 301-600/0594 Longest Harmonious Subsequence/demo.py
+++ leetcode/B 301-600/0594 Longest Harmonious Subsequence/demo.py
@@ -1,9 +1,14 @@
 from typing import List
+from collections import Counter
 
 
 class Solution(object):
     def findLHS(self, nums: List[int]) -> int:
+        dict_ = Counter(nums)
         res = 0
+        for val in dict_:
+            if val + 1 in dict_:
+                res = max(res, dict_[val] + dict_[val + 1])
         return res
 
 
