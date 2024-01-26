1577cf7a6b31d75c02896eacf93319ab97b6ca85
charlesyifanli <liyifan1400@gmail.com>
2024-01-26 23:41:46 +0800

    update leetcode--561

--- leetcode/B 301-600/0561 Array Partition/demo.py
+++ leetcode/B 301-600/0561 Array Partition/demo.py
@@ -3,12 +3,15 @@ from typing import List
 
 class Solution(object):
     def arrayPairSum(self, nums: List[int]) -> int:
-        res = 0
-        return res
+        return sum(sorted(nums)[0:len(nums):2])
 
 
 if __name__ == '__main__':
     def test():
+        # case
+        nums = [6, 2, 6, 5, 1, 2]
+        assert Solution().arrayPairSum(nums) == 9
+
         print('Succeed')
 
 
