43c6ab4220ecd31e73bc06a2f370a6218318b1b7
charlesyifanli <liyifan1400@gmail.com>
2024-01-27 22:47:28 +0800

    update leetcode--628

--- leetcode/C 601-900/0628 Maximum Product of Three Numbers/demo.py
+++ leetcode/C 601-900/0628 Maximum Product of Three Numbers/demo.py
@@ -3,12 +3,16 @@ from typing import List
 
 class Solution(object):
     def maximumProduct(self, nums: List[int]) -> int:
-        res = 0
-        return res
+        nums.sort(key=lambda x: -x)
+        return max(nums[0] * nums[1] * nums[2], nums[0] * nums[-1] * nums[-2])
 
 
 if __name__ == '__main__':
     def test():
+        # case
+        nums = [1, 2, 3, 4]
+        assert Solution().maximumProduct(nums) == 24
+
         print('Succeed')
 
 
