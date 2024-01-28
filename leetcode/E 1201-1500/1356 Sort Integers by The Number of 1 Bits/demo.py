e4c2660bf1b6226c3efd9fa6d2df7f768476efe4
charlesyifanli <liyifan1400@gmail.com>
2024-01-28 23:52:03 +0800

    update leetcode--1356

---
+++ leetcode/E 1201-1500/1356 Sort Integers by The Number of 1 Bits/demo.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution(object):
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        res = []
+        return res
+
+
+if __name__ == '__main__':
+    def test():
+        print('Succeed')
+
+
+    test()
