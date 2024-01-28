bc752966c8b7fac14226706d6307347e575cafaf
charlesyifanli <liyifan1400@gmail.com>
2024-01-28 14:39:30 +0800

    update leetcode--747

--- leetcode/C 601-900/0747 Largest Number At Least Twice of Others/demo.py
+++ leetcode/C 601-900/0747 Largest Number At Least Twice of Others/demo.py
@@ -3,13 +3,11 @@ from typing import List
 
 class Solution(object):
     def dominantIndex(self, nums: List[int]) -> int:
-        res = -1
         dict_ = dict()
         for idx, val in enumerate(nums):
             dict_[idx] = val
         sorted_list = sorted(dict_.items(), key=lambda x: -x[1])
-        if sorted_list[0][1] >= 2 * sorted_list[1][1]: res = sorted_list[0][0]
-        return res
+        return sorted_list[0][0] if sorted_list[0][1] >= 2 * sorted_list[1][1] else -1
 
 
 if __name__ == '__main__':
