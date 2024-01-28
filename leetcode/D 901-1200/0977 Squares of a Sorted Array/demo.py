43dfc535234f6e32c393061480caa6eeb06eef50
charlesyifanli <liyifan1400@gmail.com>
2024-01-28 16:52:07 +0800

    update leetcode--977

---
+++ leetcode/D 901-1200/0977 Squares of a Sorted Array/demo.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution(object):
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        res = []
+        return res
+
+
+if __name__ == '__main__':
+    def test():
+        print('Succeed')
+
+
+    test()
