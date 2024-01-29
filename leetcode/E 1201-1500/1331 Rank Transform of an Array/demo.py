17638908cf71c142e83eb66102e34f32703dc404
charlesyifanli <liyifan1400@gmail.com>
2024-01-29 15:55:53 +0800

    update leetcode--1331

--- leetcode/E 1201-1500/1331 Rank Transform of an Array/demo.py
+++ leetcode/E 1201-1500/1331 Rank Transform of an Array/demo.py
@@ -9,6 +9,10 @@ class Solution(object):
 
 if __name__ == '__main__':
     def test():
+        # case
+        arr = [40, 10, 20, 30]
+        assert Solution().arrayRankTransform(arr) == [4, 1, 2, 3]
+
         print('Succeed')
 
 
