c094ec360308f5038d08b04c77cd2e4c5f1dc5b3
charlesyifanli <liyifan1400@gmail.com>
2024-01-30 14:13:09 +0800

    update leetcode--1346

--- leetcode/E 1201-1500/1346 Check If N and Its Double Exist/demo.py
+++ leetcode/E 1201-1500/1346 Check If N and Its Double Exist/demo.py
@@ -3,12 +3,27 @@ from typing import List
 
 class Solution(object):
     def checkIfExist(self, arr: List[int]) -> bool:
-        res = True
-        return res
+        set_ = set()
+        for i in range(len(arr)):
+            if 2 * arr[i] in set_ or arr[i] / 2 in set_:
+                return True
+            else:
+                set_.add(arr[i])
+        return False
 
 
 if __name__ == '__main__':
     def test():
+        # case
+        arr = [7, 1, 14, 11]
+        assert Solution().checkIfExist(arr) == True
+
+        arr = [-10, 12, -20]
+        assert Solution().checkIfExist(arr) == True
+
+        arr = [0, 0]
+        assert Solution().checkIfExist(arr) == True
+
         print('Succeed')
 
 
