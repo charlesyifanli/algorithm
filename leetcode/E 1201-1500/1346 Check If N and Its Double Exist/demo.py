df8bfea6e5a506a932c0f9e630cb81b6668af08c
charlesyifanli <liyifan1400@gmail.com>
2024-01-30 13:41:15 +0800

    update leetcode--1346

---
+++ leetcode/E 1201-1500/1346 Check If N and Its Double Exist/demo.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution(object):
+    def checkIfExist(self, arr: List[int]) -> bool:
+        res = True
+        return res
+
+
+if __name__ == '__main__':
+    def test():
+        print('Succeed')
+
+
+    test()
