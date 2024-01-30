4d0a0d6f057c92979921385862ec2ef38c374b9e
charlesyifanli <liyifan1400@gmail.com>
2024-01-30 19:56:31 +0800

    update leetcode--1207

---
+++ leetcode/E 1201-1500/1207 Unique Number of Occurrences/demo.js
@@ -0,0 +1,12 @@
+/**
+ * @param arr {number[]}
+ * @return {boolean}
+ */
+var uniqueOccurrences = function (arr) {
+    var counts = arr.reduce(function (acc, num) {
+        acc[num] = (acc[num] || 0) + 1
+        return acc
+    }, {})
+    var values = Object.values(counts)
+    return new Set(values).size === values.length
+}
