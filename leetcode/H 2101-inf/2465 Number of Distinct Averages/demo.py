deda49ec6f6865e669af1ae3715014c5369fc6f6
charlesyifanli <liyifan1400@gmail.com>
2024-01-31 14:09:11 +0800

    update leetcode--2465

---
+++ leetcode/H 2101-inf/2465 Number of Distinct Averages/demo.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution(object):
+    def distinctAverages(self, nums: List[int]) -> int:
+        res = 0
+        return res
+
+
+if __name__ == '__main__':
+    def test():
+        print('Succeed')
+
+
+    test()
