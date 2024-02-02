63ccfd8eb0e633201230a537964f0911bffd5cb1
charlesyifanli <liyifan1400@gmail.com>
2024-02-02 18:36:49 +0800

    update leetcode--2148

--- leetcode/H 2101-inf/2148 Count Elements With Strictly Smaller and Greater Elements/demo.py
+++ leetcode/H 2101-inf/2148 Count Elements With Strictly Smaller and Greater Elements/demo.py
@@ -1,14 +1,26 @@
 from typing import List
+from collections import Counter
 
 
 class Solution(object):
     def countElements(self, nums: List[int]) -> int:
         res = 0
+        sorted_list = sorted(list(dict(Counter(nums)).items()), key=lambda x: x[0])
+        for i in range(1, len(sorted_list) - 1):
+            res += sorted_list[i][1]
         return res
 
 
 if __name__ == '__main__':
     def test():
+        # case
+        nums = [-3, 3, 3, 90]
+        assert Solution().countElements(nums) == 2
+
+        # case
+        nums = [11, 7, 2, 15]
+        assert Solution().countElements(nums) == 2
+
         print('Succeed')
 
 
