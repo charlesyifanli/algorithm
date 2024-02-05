4bf7774205639859678195a6f90571fe064ea554
charlesyifanli <liyifan1400@gmail.com>
2024-02-05 23:37:11 +0800

    update leetcode--14

---
+++ leetcode/A 1-300/0014 Longest Common Prefix/demo.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution(object):
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ''
+        return res
+
+
+if __name__ == '__main__':
+    def test():
+        print('Succeed')
+
+
+    test()
