042e6f4432f054a36d797bbb817372a640e9e4e0
charlesyifanli <liyifan1400@gmail.com>
2024-02-08 15:58:48 +0800

    update lanqiaocup--3179

--- lanqiaocup/2023/3179 Average/demo.py
+++ lanqiaocup/2023/3179 Average/demo.py
@@ -1,2 +1,25 @@
 class Solution(object):
     def function(self) -> None:
+        n = int(input())
+        res = 0
+        dict_count, dict_cost = {}, {}
+
+        for i in range(n):
+            a, b = list(map(int, input().split()))
+            if a not in dict_cost:
+                dict_count[a] = 1
+                dict_cost[a] = [b]
+            else:
+                dict_count[a] += 1
+                dict_cost[a].append(b)
+
+        target = n // 10
+        for key, val in dict_count.items():
+            if val > target:
+                dict_cost[key].sort()
+                res += sum(dict_cost[key][:val - target])
+
+        print(res)
+
+
+Solution().function()
