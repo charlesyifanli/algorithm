79cb9610776e749946a54b9032922ddedb5171b9
charlesyifanli <liyifan1400@gmail.com>
2024-02-08 14:21:13 +0800

    update lanqiaocup--2375

--- lanqiaocup/2022/2375 Model Taking/demo.py
+++ lanqiaocup/2022/2375 Model Taking/demo.py
@@ -1,21 +1,17 @@
 class Solution(object):
-    a = int(input())
-    def same_add(m, n):
+    def take_model(self):
+        count = int(input())
+        res = []
+        for i in range(count):
+            n, m = list(map(int, input().split()))
+            res.append(self.helper(n, m))
+        for val in res:
+            print(val)
+
+    def helper(self, n: int, m: int) -> str:
         for i in range(1, m + 1):
-            if n % i != i - 1:
-                return "Yes"
-        return "No"
-    for i in range(a):
-        n, m = list(map(int, input().split(" ")))
-        print(same_add(m, n))
-
-    # def take_model(self):
-    #     res = []
-    #     n = int(input())
-    #     for i in range(n):
-    #         list_ = list(map(int, input().split()))
-    #         n, m = list_[0], list_[1]
-
+            if n % i != i - 1: return 'Yes'
+        return 'No'
 
 
 Solution().take_model()
