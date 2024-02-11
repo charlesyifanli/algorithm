04aebac10e856639be648e273f4481716afda93c
charlesyifanli <liyifan1400@gmail.com>
2024-02-11 20:14:26 +0800

    update  lanqiaocup\quize\test1\Digital Triangle

--- lanqiaocup/quize/test1/Digital Triangle/demo.py
+++ lanqiaocup/quize/test1/Digital Triangle/demo.py
@@ -1,6 +1,3 @@
-from typing import List
-
-
 class Solution(object):
     def function(self) -> None:
         n = int(input())
@@ -8,6 +5,20 @@ class Solution(object):
         for i in range(n):
             curr = [int(x) for x in input().split()]
             ls.append(curr)
+        for i in range(n):
+            for j in range(i + 1):
+                if i == 0 and j == 0:
+                    dp[i][j] = ls[i][j]
+                elif j == 0:
+                    dp[i][j] = ls[i][j] + dp[i - 1][j]
+                elif j == i:
+                    dp[i][j] = ls[i][j] + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = ls[i][j] + max(dp[i - 1][j], dp[i - 1][j - 1])
+        if n % 2 == 0:
+            print(max(dp[n - 1][n // 2], dp[n - 1][n // 2 + 1]))
+        else:
+            print(dp[n - 1][n // 2])
 
 
 Solution().function()
