c67af84b0b9e587d58fd8aa8c873ab0a0b204d1b
charlesyifanli <liyifan1400@gmail.com>
2024-02-11 19:49:38 +0800

    update  lanqiaocup\quize\test1\Digital Triangle

--- lanqiaocup/quize/test1/Digital Triangle/demo.py
+++ lanqiaocup/quize/test1/Digital Triangle/demo.py
@@ -1,1 +1,13 @@
+from typing import List
 
+
+class Solution(object):
+    def function(self) -> None:
+        n = int(input())
+        ls, dp = [], [[0 for j in range(i + 1)] for i in range(n)]
+        for i in range(n):
+            curr = [int(x) for x in input().split()]
+            ls.append(curr)
+
+
+Solution().function()
