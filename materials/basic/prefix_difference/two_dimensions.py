4085375e2ed3b51585986b2c3ee8aed11053b0e1
charlesyifanli <liyifan1400@gmail.com>
2024-03-01 17:10:10 +0800

    update materials -- prefix-diff --2D pref and diff --and sum

--- materials/basic/prefix_difference/two_dimensions.py
+++ materials/basic/prefix_difference/two_dimensions.py
@@ -26,12 +26,47 @@ def get_2d_diff(arr: list) -> list:
     return diff
 
 
-def get_2d_sum(arr: list, x1: int, y1: int, x2: int, y2: int) -> int:
+def regional_2d_sum(arr: list, top_left: list[int], bottom_right: list[int]) -> int:
+    x1, y1 = top_left
+    x2, y2 = bottom_right
     pref = get_2d_pref(arr)
-    print(pref)
     return pref[x2][y2] - pref[x2][y1 - 1] - pref[x1 - 1][y2] + pref[x1 - 1][y1 - 1]
+
+
+def regional_2d_add_minus(arr: list, top_left: list[int], bottom_right: list[int], num: int) -> list:
+    rows, cols = len(arr), len(arr[0])
+
+    # 获取差分数组
+    diff = get_2d_diff(arr)
+
+    # 确定要修改的区域
+    row_start, col_start = top_left
+    row_end, col_end = bottom_right
+
+    # 更新差分数组以实现加减操作
+    diff[row_start][col_start] += num
+    if row_end + 1 < rows:
+        diff[row_end + 1][col_start] -= num
+    if col_end + 1 < cols:
+        diff[row_start][col_end + 1] -= num
+    if row_end + 1 < rows and col_end + 1 < cols:
+        diff[row_end + 1][col_end + 1] += num
+
+    # 从差分数组构建原始二维数组
+    for i in range(rows):
+        for j in range(cols):
+            if i == 0 and j == 0:
+                arr[i][j] = diff[i][j]
+            elif i == 0:
+                arr[i][j] = diff[i][j] + arr[i][j - 1]
+            elif j == 0:
+                arr[i][j] = diff[i][j] + arr[i - 1][j]
+            else:
+                arr[i][j] = diff[i][j] + arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1]
+
+    return arr
 
 
 arr = [[1, 2], [2, 3], [3, 4]]
 
-print(get_2d_sum(arr, 1, 1, 2, 1))
+print(regional_2d_add_minus(arr, [2, 1], [2, 1], 1))
