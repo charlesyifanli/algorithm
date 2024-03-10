3fed1b238dedc78ed246dcfc9526f1a98faad557
charlesyifanli <liyifan1400@gmail.com>
2024-03-10 21:49:12 +0800

    update leetcode 198

--- leetcode/A 1-300/0198 House Robber/demo.py
+++ leetcode/A 1-300/0198 House Robber/demo.py
@@ -1,13 +1,18 @@
 class Solution:
     def rob(self, nums: list[int]) -> int:
-        dp = [[0, nums[0]] for _ in range(len(nums))]
-        for i in range(1, len(nums)):
-            dp[i][0] = dp[i - 1][1]
-            dp[i][1] = dp[i - 1][0] + nums[i]
-        return max(dp[-1])
+        if len(nums) < 3:
+            return max(nums[:2])
+        dp = [0 for _ in range(len(nums))]
+        dp[0], dp[1] = nums[0], max(nums[:2])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+        return dp[-1]
 
 
 if __name__ == '__main__':
+    assert Solution().rob([0]) == 0
     assert Solution().rob([2, 1, 1, 2]) == 4
     assert Solution().rob([1, 2, 3, 1]) == 4
     assert Solution().rob([2, 7, 9, 3, 1]) == 12
