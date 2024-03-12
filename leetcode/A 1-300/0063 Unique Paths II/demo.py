6390d04d40d48e3875f0e5603302d79176d26fab
charlesyifanli <liyifan1400@gmail.com>
2024-03-12 15:59:03 +0800

    update leetcode 63

--- leetcode/A 1-300/0063 Unique Paths II/demo.py
+++ leetcode/A 1-300/0063 Unique Paths II/demo.py
@@ -1,25 +1,21 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: list[list[int]]) -> int:
-        dp = [[0 for _ in range(len(obstacleGrid[0]))] for _ in range(len(obstacleGrid))]
-        dp[0][0] = 1 if obstacleGrid[0][0] == 0 else 0
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
 
-        for i in range(len(obstacleGrid)):
-            for j in range(len(obstacleGrid[0])):
-                if i == j == 0:
-                    continue
-                elif i == 0:
-                    if obstacleGrid[i][j] == 1:
-                        dp[i][j] = 0
-                    else:
-                        dp[i][j] = dp[i][j - 1]
-                elif j == 0:
-                    if obstacleGrid[i][j] == 1:
-                        dp[i][j] = 0
-                    else:
-                        dp[i][j] = dp[i - 1][j]
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1 - obstacleGrid[0][0]
+
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
                 else:
-                    if obstacleGrid[i][j] == 1:
-                        dp[i][j] = 0
+                    if i == j == 0:
+                        continue
+                    elif i == 0:
+                        dp[i][j] = dp[i][j - 1]
+                    elif j == 0:
+                        dp[i][j] = dp[i - 1][j]
                     else:
                         dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
 
