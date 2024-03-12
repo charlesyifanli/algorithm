4c124cca7d27b29d791c3dc80f781ec6a0debf29
charlesyifanli <liyifan1400@gmail.com>
2024-03-12 15:54:41 +0800

    update leetcode 63

--- leetcode/A 1-300/0063 Unique Paths II/demo.py
+++ leetcode/A 1-300/0063 Unique Paths II/demo.py
@@ -26,6 +26,25 @@ class Solution:
         return dp[-1][-1]
 
 
+# class Solution:
+#     def uniquePathsWithObstacles(self, obstacleGrid: list[list[int]]) -> int:
+#         m, n = len(obstacleGrid), len(obstacleGrid[0])
+#         dp = [[0] * n for _ in range(m)]
+#         dp[0][0] = 1 - obstacleGrid[0][0]
+#
+#         for i in range(m):
+#             for j in range(n):
+#                 if obstacleGrid[i][j] == 1:
+#                     dp[i][j] = 0
+#                 else:
+#                     if i > 0:
+#                         dp[i][j] += dp[i - 1][j]
+#                     if j > 0:
+#                         dp[i][j] += dp[i][j - 1]
+#
+#         return dp[-1][-1]
+
+
 if __name__ == '__main__':
     assert Solution().uniquePathsWithObstacles([[0, 0, 0], [0, 1, 0], [0, 0, 0]]) == 2
     assert Solution().uniquePathsWithObstacles([[0, 1], [0, 0]]) == 1
